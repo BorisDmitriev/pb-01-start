9683b261603b73e00112a80de6a3990fb111159d
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:38:51 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,13 @@
+// write your code in this file
+
+console.log("This is an exercise in console logging");
+const exercise = "This is the value.";
+console.log(exercise);
+const firstName = "John";
+const lastName = "Doe";
+console.log(firstName, lastName);
+const city = "Woshington";
+const age = "35";
+const job = "Developer";
+console.log(firstName, lastName, city, age, job);
+console.log(firstName +  " " + lastName + " is a " + age + "-year-old teacher who lives in Berlin.");
